1e6fb5af951a859c4c4d1dacfc2ad1a60b6eba9a
Vishnu Elayi <vishnuelayi@gmail.com>
2024-06-22 15:34:06 +0530

    first two simple problems

---
+++ swap-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+    int temp = 0;
+    
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << endl;
+    cout << b;
+
+    return 0;
+}
